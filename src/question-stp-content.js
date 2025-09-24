32291929651e9fe2c1adafa6912b977376ce4448
Sam Rabiyah <sraby@users.noreply.github.com>
2025-09-24 13:10:25 -0400

    Separate sample content into each city document

--- src/question-stp-content.js
+++ src/question-stp-content.js
@@ -1,1 +1,219 @@
-export const questionStpContent = {"question1":{"title":"Why are you running for mayor?","subject":"tk","tellMeMore":"tk","option1":"tk","option2":"tk","option3":"tk","option4":"tk"},"question2":{"title":"As mayor, how will you advocate for immigrants and communities of color?","subject":"tk","tellMeMore":"tk","option1":"tk","option2":"tk","option3":"tk","option4":"tk"},"question3":{"title":"Do you support the city’s sanctuary policy, and should it be expanded?","subject":"Immigration.","tellMeMore":"St. Paul city policy prohibits local law enforcement from asking people about their immigration status or for their immigration documents. The St. Paul Police Department is also not allowed to cooperate with U.S. Immigration and Customs Enforcement on federal immigration enforcement.","option1":"Yes, and it should be expanded.","option2":"Yes, and it is fine as it is.","option3":"No."},"question4":{"title":"The corridor in Midway starting at Snelling Ave and University Ave has lost or is losing several businesses. What should the mayor do to revive this corridor, which is home to many communities of color?","subject":"Economic development","tellMeMore":"The Midway corridor, home to many immigrants and communities of color, is struggling after the closure of several big box stores and grocery stores, including TJ Maxx, Herbergers, Cub Foods and WalMart.","option1":"Improve public safety.","Option2":"Be more welcoming to businesses.","option3":"Economic investment.","option4":"Institute a land value tax.","Option5":"Lower the city’s sales and property taxes."},"question5":{"title":"How can the city make up for lost federal grants and revenue due to cuts from the Trump administration?","subject":"tk","tellMeMore":"tk","option1":"Spend city money more efficiently.","option2":"Leverage relationship with state Legislature for more state funding.","option3":"Encourage more development.","Option4":"Reform the city’s business policies."},"question6":{"title":"Should the city cut services as one way to address loss of federal funds and dwindling downtown tax base?","subject":"","tellMeMore":"The Trump administration has slashed several federal grants and funds that go directly to cities like St. Paul. At the same time, St. Paul’s downtown buildings are largely vacant since the pandemic changed how workplaces use offices, leading to less tax revenue for the city.","option1":"Yes","option2":"No","option3":"Maybe","option4":"Don’t know"},"question7":{"title":"Should the state require its employees to return to the office fulltime to help revive downtown?","subject":"Business and economy","tellMeMore":"St. Paul is home to many state government offices. This last summer, Gov. Tim Walz required state employees to return to the office to work for at least half of the workweek for the first time since the pandemic started. The move is meant to help revitalize businesses reliant on office workers.","option1":"Yes","option2":"No","option3":"Maybe","option4":"Don’t know"},"question8":{"title":"How can the city revive downtown, which is home to several vacant offices?","subject":"Economic Development","tellMeMore":"tk","option1":"tk","option2":"tk","option3":"tk","option4":"tk"},"question9":{"title":"Should the city spend money to remodel the Grand Casino Arena (formerly Xcel Energy Center) that the Minnesota Wild is requesting?","subject":"Stadium subsidies","tellMeMore":"The Minnesota Wild are asking for $394 million from the state to renovate the 25-year-old hockey arena. A renovation would keep the Wild in downtown St. Paul.","option1":"Yes","option2":"No","option3":"Maybe","option4":"Don’t know"},"question10":{"title":"Should the city stop collecting trash and revert to the previous practice of leaving it up to residents to organize their own trash collection?","subject":"tk","tellMeMore":"tk","option1":"Yes","option2":"No","option3":"Maybe","option4":"Don’t know"},"question11":{"title":"Do you support the St. Paul rent control policy as it now stands? Does it need more exemptions for certain types of residences?","subject":"Rent Contol","tellMeMore":"tk","option1":"The city should keep rent control but add more exemptions.","option2":"The current rent control policy is adequate","option3":"The city should repeal rent control altogether","option4":"The city should go back to the original rent control policy that voters passed in 2023."},"question12":{"title":"Should the city look into charging fees to tax-exempt large nonprofits, hospitals and universities as a new way to raise revenue?","subject":"tk","tellMeMore":"tk","option1":"Yes","option2":"No","option3":"Maybe","option4":"Don’t know"},"question13":{"title":"Should the state revoke the operation permit for Northern Iron, a metal foundry on the East Side cited for pollution violations?","subject":"tk","tellMeMore":"tk","option1":"Yes","option2":"No","option3":"Maybe","option4":"Don’t know"},"question14":{"title":"How can the city best prevent another cyberattack?","subject":"tk","tellMeMore":"tk","option1":"tk","option2":"tk","option3":"tk","option4":"tk"},"question15":{"title":"Should St. Paul Public Works plow alleys instead of leaving it up to residents?","subject":"tk","tellMeMore":"tk","option1":"Yes","option2":"No","option3":"Maybe","option4":"Don’t know"}}
+export const questionStpContent = {
+  question1: {
+    title: "What is your preferred model for outdoor dining?",
+    subject: "Streets",
+    tellMeMore:
+      'Owners of cafes and restaurants say they are <a href="https://www.nytimes.com/2025/04/14/nyregion/nyc-restaurants-outdoor-seating.html">struggling to participate</a> in a new seasonal outdoor dining program advanced by the City Council, which requires city permits and operates under stricter rules than those that prevailed during the COVID emergency.',
+    option1:
+      "Bring back year-round outdoor dining as it existed during the pandemic",
+    option2: "Maintain the current permit program approved by the City Council",
+    option3:
+      "Consult with restaurant owners and propose a new permit program tailored to their needs",
+    option4: "",
+  },
+  question2: {
+    title: "Which best describes your approach to bike lanes?",
+    subject: "Streets",
+    tellMeMore:
+      '<a href="https://ibo.nyc.ny.us/iboreports/local-law-195-explainer-february-2025.pdf">Local Law 195</a> mandates that the Department of Transportation install 250 miles of protected bike lanes by 2026. The agency has yet to install enough lanes to meet its own 50-mile annual target, often falling short by some 20 miles each year.',
+    option1:
+      "Continue to expand the bike lane network and provide more protected lanes",
+    option2: "Keep the bike lane network as is",
+    option3: "Remove bike lanes where they are not heavily used",
+    option4: "",
+  },
+  question3: {
+    title: "Do you support trash containerization?",
+    subject: "Streets",
+    tellMeMore:
+      'The Department of Sanitation is <a href="https://www.thecity.nyc/2024/12/04/trash-bin-questions-answered-garbage/">rolling out rules</a> requiring household trash to be put into containers for curbside collection. Residences with fewer than 10 apartments must place garbage inside individual bins for pickup, and an upper Manhattan pilot is underway of big bins for larger buildings.',
+    option1:
+      "Requiring property owners to use standard containers is overreach and should be rolled back",
+    option2: "Only small properties should use standardized containers",
+    option3:
+      "Following the test pilot, roll out standardized containerization for larger buildings but reverse the requirement for small properties",
+    option4: "Promote containerization for all residential buildings",
+  },
+  question4: {
+    title: "Which statement best reflects your preferred approach to policing?",
+    subject: "Public Safety",
+    tellMeMore:
+      'All major categories of crime are far above <a href="https://www.vitalcitynyc.org/articles/vital-signs-state-of-the-city-on-crime-2024">pre-pandemic levels</a>, especially felony assaults, while the number of NYPD officers has declined below the department’s budgeted headcount. The current mayor has intensified <a href="https://www.thecity.nyc/2023/09/12/nypd-quality-of-life-crackdown-enforcement-skyrockets-criminal-court/">criminal enforcement</a> of quality-of-life offenses.',
+    option1:
+      "Increase the size of the police force and ramp up enforcement of quality-of-life violations as well as more serious crimes",
+    option2:
+      "Maintain roughly the current size of the police force and focus enforcement on serious crimes",
+    option3:
+      "Scale back the police force and focus on improving New Yorkers’ economic prosperity and access to public goods",
+    option4: "",
+  },
+  question5: {
+    title: "How should the NYPD enforce transit fares?",
+    subject: "Public Safety",
+    tellMeMore:
+      'The state-run Metropolitan Transportation Authority estimated that in 2022 it lost $600 million in revenue due to subway and bus fare evasion. The MTA reported a <a href="https://gothamist.com/news/mta-reports-dip-in-fare-evasion-as-nypd-arrests-skyrocket-in-subway-system?gad_source=1&gbraid=0AAAAAD9J8hHe2SwLleiW8C4K41XuGz8JL&gclid=Cj0KCQjw2ZfABhDBARIsAHFTxGzF0WiI-DTRimsPaPfQRnN2MaHiJddO3eByk6nKll3DQbIjFdgZZ4waAnzeEALw_wcB">modest decrease</a> in fare evasion during the last three months of 2024, even as fare evasion arrests <a href="https://www.nyc.gov/site/nypd/stats/reports-analysis/subway-fare-evasion.page">more than doubled</a> compared with the same period a year earlier.',
+    option1:
+      "Consistent summonses for violators — maintain or expand fare enforcement",
+    option2: "Fewer summonses for violators — enforcement is too aggressive",
+    option3: "Do not issue summonses for fare nonpayment",
+    option4: "",
+  },
+  question5a: {
+    title:
+      "The number of people incarcerated at Rikers Island is approaching double the capacity of planned new jails. What should the next mayor do?",
+    subject: "Public Safety",
+    tellMeMore:
+      'The four new borough-based jails – meant to replace the Rikers Island complex and place incarcerated folks in areas closer to their families and courts — will have 4,160 beds. As of March, 7,067 people were <a href="https://www.thecity.nyc/2025/03/20/rikers-population-tops-7000/">incarcerated at Rikers</a>.',
+    option1:
+      "Stay on course with the Rikers Island shutdown, work with DAs to release more people pretrial or divert them from prosecution entirely",
+    option2:
+      "Stay on course with the Rikers Island shutdown, work with the City Council to keep a Rikers facility open to maintain continued capacity",
+    option3:
+      "Pause planning for jails still in the design phase, work with the City Council to reinvest in Rikers facilities",
+    option4: "",
+  },
+  question6: {
+    title: "What is your preferred path to creating more affordable housing?",
+    subject: "Housing",
+    tellMeMore:
+      'New York City is short by half a million homes <a href="https://rpa.org/work/reports/city-of-yes-and-missing-middle-housing">by some estimates</a>, and over half of the city’s households are rent-burdened –— spending more than 30% of their income on housing.',
+    option1:
+      "Push to expand development rights in every community district, with existing affordable housing set-asides",
+    option2:
+      "Provide subsidies to renovate existing apartments and office space",
+    option3: "Lower taxes and other development costs",
+    option4:
+      "Promote policies that give renters more control over their housing and curb real estate speculation",
+  },
+  question7: {
+    title: "How should the next mayor manage decaying public housing?",
+    subject: "Housing",
+    tellMeMore:
+      "The New York City Housing Authority operates under a federal monitor who is overseeing repairs. Dozens of developments now operate under private management via programs that invest in upgrades, including Rental Assistance Demonstration, or “RAD.”",
+    option1:
+      "Public housing is public. Stop bringing in private management to take over",
+    option2: "Consider private management for existing public housing",
+    option3: "Pursue new development on NYCHA property with private landlords",
+    option4: "",
+  },
+  question8: {
+    title: "What should the Rent Guidelines Board do next year?",
+    subject: "Housing",
+    tellMeMore:
+      'Each June, a panel selected by the mayor, representing property owners, tenants and the public, votes on whether to increase the rent on <a href="https://rentguidelinesboard.cityofnewyork.us/resources/faqs/rent-control/#:~:text=Yes.,on%20their%20Rent%20Control%20page:">some 960,000 rent-stabilized units</a> and by how much. The board <a href="https://rentguidelinesboard.cityofnewyork.us/2025-summary-of-proposed-apartment-loft-hotel-guidelines/">proposed</a> in April to increase rents of one-year leases by as much as 4.75% and two-year leases by as much as 7.75%.',
+    option1: "Freeze rents, period",
+    option2: "Freeze rents if economic analysis determines it's feasible",
+    option3:
+      "The Rent Guidelines Board should set rent increases, in line with landlords' actual costs",
+    option4: "",
+  },
+  question9: {
+    title: "What’s the best way to admit students to specialized high schools?",
+    subject: "Schools",
+    tellMeMore:
+      'State <a href="https://nebula.wsimg.com/75694f5e724770756e3a1c3e229c2616?AccessKeyId=45A6F09DA41DB93D9538&disposition=0&alloworigin=1">law</a> requires three coveted specialized high schools to admit students based solely on a single exam’s scores. (Five other schools have adopted the same admissions procedure.) But the results are inequitable: less than 5% of offers last year <a href="https://www.chalkbeat.org/newyork/2024/06/18/specialized-high-school-offers-to-black-and-latino-students-increase-but-remain-low/">went to Black students</a>, while 7.6% went to Latino students, far lower than their population share.',
+    option1:
+      "Maintain the SHSAT as the exclusive test for all of the eight schools where it’s currently used for admission",
+    option2:
+      "Maintain the SHSAT only for the three schools where it’s required by state law (Bronx Science, Brooklyn Tech, Stuyvesant)",
+    option3:
+      "Demand state lawmakers remove the SHSAT as the sole path to admissions to the three schools",
+    option4: "",
+  },
+  question10: {
+    title:
+      "Just 28% of fourth graders are proficient in reading. What should the next mayor do to improve learning?",
+    subject: "Schools",
+    tellMeMore:
+      "All public elementary schools are now teaching students how to read using city-mandated curricula, aimed at improving literacy rates among the youngest students. Educators typically promote students to the next grade level after a review of their work from the current school year.",
+    option1: "Stay on the current course, since scores are gradually improving",
+    option2:
+      "Only promote children to the next grade once they can pass the reading test",
+    option3: "Reduce standardized testing in city public schools",
+    option4: "",
+  },
+  question12: {
+    title: "What is your position on NYC’s sanctuary city laws?",
+    subject: "Federal Government",
+    tellMeMore:
+      "NYC laws prohibit most cooperation with immigration enforcement, making exceptions only for the NYPD and Department of Correction to transfer custody where someone sought by federal agents has been convicted of a specified violent crime.",
+    option1:
+      "Laws should remain as they are: City authorities should only cooperate with federal agents on immigration enforcement where a person has previously been convicted of a qualifying crime, regardless of new charges",
+    option2:
+      "Change the laws: City authorities should help federal agents with enforcement for people charged with violent felonies, in addition to those already convicted",
+    option3:
+      "Change the laws: City authorities should cooperate with ICE as demanded regardless of a person's history with the justice system",
+    option4:
+      "Change the laws: City authorities should further restrict cooperation with federal authorities",
+  },
+  question13: {
+    title: "How should the next mayor deal with President Trump?",
+    subject: "Federal Government",
+    tellMeMore:
+      "President Donald Trump has taken numerous steps targeting New York City specifically, including threatening to end congestion pricing that helps fund mass transit and clawing back funds the city uses to aid migrants.",
+    option1: "Seek partnership and focus on benefits",
+    option2: "Resistance is the priority",
+    option3:
+      "Neutrality: focus on the city and let the governor and congressional delegation deal with the White House",
+    option4: "",
+  },
+  question14: {
+    title:
+      "What is your spending priority for $500 million in opioid settlement funds?",
+    subject: "Health",
+    tellMeMore:
+      'Under settlements reached with pharmaceutical companies, the state and city have <a href="https://www.nyc.gov/office-of-the-mayor/news/683-24/mayor-adams-ramp-up-annual-support-50-million-opioid-prevention-treatment-major">amassed funds</a> to aid people with substance use disorders. By 2040, the city anticipates that it will receive a total of $500 million.',
+    option1:
+      "Supportive housing services for people with substance use disorders",
+    option2:
+      "Safe injection facilities and needle exchanges to limit fatalities among users",
+    option3: "Law enforcement efforts to crack down on drug dealers",
+    option4: "",
+  },
+  question15: {
+    title:
+      "Do you support involuntary hospitalization for people with volatile episodes of mental illness?",
+    subject: "Health",
+    tellMeMore:
+      "New changes to the state’s involuntary commitment law make it easier for police and health professionals to admit people with mental illness — even without evidence of imminent physical harm to themselves or others — to hospitals.",
+    option1:
+      "I support the law’s premise: It needs to be easier to hospitalize people when warranted.",
+    option2:
+      "I oppose the law’s premise: People’s rights to make their own mental health care decisions should not be compromised",
+    option3: "I support the bill's premise but it does not go far enough",
+    option4: "",
+  },
+  question17: {
+    title: "What kind of manager should the next mayor be?",
+    subject: "Leadership",
+    tellMeMore: "",
+    option1:
+      "The Decider — centralize decision-making with the mayor and close advisers",
+    option2:
+      "The Partner — make decisions in coordination with the City Council",
+    option3:
+      "The Delegator — hire top talent to run agencies and rely on their judgment",
+    option4: "",
+  },
+  question18: {
+    title:
+      "A generation from now, what’s the one legacy you’d like New Yorkers to recall from the next mayor?",
+    subject: "Leadership",
+    tellMeMore: "",
+    option1:
+      "They expanded opportunities for poor and working-class New Yorkers",
+    option2: "They created lasting public works",
+    option3: "They imposed fiscal discipline and cleaned up government",
+    option4: "They curbed disorder and improved neighborhood quality of life",
+  },
+  question19: {
+    title:
+      "What kind of early-career experience is most important for the next mayor to have?",
+    subject: "Leadership",
+    tellMeMore: "",
+    option1: "Led a nonprofit",
+    option2: "Served as a community or labor organizer",
+    option3: "Worked in the private sector",
+    option4: "Career-long work in government or politics",
+  },
+};
