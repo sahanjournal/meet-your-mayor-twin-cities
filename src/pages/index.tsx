8067b02270e1b794a72b78e00576c8f49e4999d4
Sam Rabiyah <sraby@users.noreply.github.com>
2025-09-24 15:19:03 -0400

    Wrap redirect page with a PageLayout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { navigate } from "gatsby";
+import { PageLayout } from "../components/PageLayout";
 
 /**
  * The path that users will be redirected to when they visit the root URL.
@@ -16,17 +17,19 @@ const RedirectPage = () => {
   }, []);
 
   return (
-    <section className="hero is-white is-fullheight is-flex is-justify-content-center is-align-items-center">
-      <div className="has-text-centered">
-        <button
-          className="button is-loading is-large is-white"
-          style={{ border: "none", boxShadow: "none" }}
-        >
-          {/* Invisible button just to use Bulma's loading spinner */}
-        </button>
-        <p className="eyebrow mt-3">Redirecting...</p>
-      </div>
-    </section>
+    <PageLayout>
+      <section className="hero is-white is-fullheight is-flex is-justify-content-center is-align-items-center">
+        <div className="has-text-centered">
+          <button
+            className="button is-loading is-large is-white"
+            style={{ border: "none", boxShadow: "none" }}
+          >
+            {/* Invisible button just to use Bulma's loading spinner */}
+          </button>
+          <p className="eyebrow mt-3">Redirecting...</p>
+        </div>
+      </section>
+    </PageLayout>
   );
 };
 
