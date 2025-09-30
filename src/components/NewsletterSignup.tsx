1a9056e5eb0ff1f2aa0fcb6bf904703200ce4162
Sam Rabiyah <sraby@users.noreply.github.com>
2025-09-30 13:34:36 -0400

    Add Newsletter config (#40)

--- src/components/NewsletterSignup.tsx
+++ src/components/NewsletterSignup.tsx
@@ -1,105 +1,59 @@
 import React, { useState, FormEvent, ChangeEvent } from "react";
 import { OutboundLink } from "./Links";
-import { useLocation } from "@reach/router";
 import classnames from "classnames";
+import { City, useCity } from "../utils";
 
-const GOTHAMIST_EMAIL_LIST_NAME =
-  "Gothamist Membership++Politics Brief Newsletter";
-
-const THE_CITY_FALLBACK_NEWSLETTER_LINK =
-  "https://www.thecity.nyc/newsletter-mayoral-election-2025-eric-adams/";
-
-const GOTHAMIST_FALLBACK_NEWSLETTER_LINK = "https://gothamist.com/newsletters";
+const SAHAN_FALLBACK_NEWSLETTER_LINK = "https://sahanjournal.com/newsletter/";
 
 export type RequestStatus = "idle" | "loading" | "success" | "error";
 
 export const NewsletterSignupBanner: React.FC<{
   isOnLandingPage?: boolean;
 }> = ({ isOnLandingPage }) => {
+  const city = useCity();
   const [email, setEmail] = useState<string>("");
-  const [statusTheCity, setStatusTheCity] = useState<RequestStatus>("idle");
-  const [statusGothamist, setStatusGothamist] = useState<RequestStatus>("idle");
-
-  const location = useLocation();
-  /**
-   * Let's use the DEMO API from Gothamist if we are in local development or
-   * on the staging site:
-   */
-  const isTestingSite =
-    location?.origin?.includes("localhost") ||
-    location?.origin?.includes("qa-project.thecity.nyc");
-  const gothamistApiUrl = isTestingSite
-    ? "https://api.demo.nypr.digital/email-proxy/subscribe"
-    : "https://api.prod.nypr.digital/email-proxy/subscribe";
+  const [statusSahan, setStatusSahan] = useState<RequestStatus>("idle");
 
   /**
-   * Sign up for THE CITY's Ranked Choices newsletter via Netlify email proxy
+   * Sign up for Sahan Journal's newsletter via direct API request.
    */
-  const submitTheCity = async (e: FormEvent<HTMLFormElement>) => {
+  const submitSahan = async (e: FormEvent<HTMLFormElement>, city: City) => {
     e.preventDefault();
-    setStatusTheCity("loading");
+    setStatusSahan("loading");
     try {
       const response = await fetch(
-        "https://rankedchoices-proxy.netlify.app/.netlify/functions/subscribe",
+        "https://sahan-mail.netlify.app/.netlify/functions/subscribe",
         {
           method: "POST",
           headers: {
-            accept: "application/json",
             "content-type": "application/json",
           },
           body: JSON.stringify({
             email,
+            city,
           }),
         }
       );
 
       if (response.ok) {
-        setStatusTheCity("success");
+        setStatusSahan("success");
         setEmail("");
       } else {
-        setStatusTheCity("error");
+        setStatusSahan("error");
       }
     } catch (error) {
-      setStatusTheCity("error");
+      setStatusSahan("error");
     }
   };
 
-  /**
-   * Sign up for Gothamist's politics newsletter via their custom email proxy
-   */
-  const submitGothamist = async (e: FormEvent<HTMLFormElement>) => {
-    try {
-      const response = await fetch(gothamistApiUrl, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          source: "meet_your_mayor",
-          list: GOTHAMIST_EMAIL_LIST_NAME,
-          email,
-        }),
-      });
+  const handleSubmit =
+    (city: City) => async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
 
-      if (response.ok) {
-        setStatusGothamist("success");
-      } else {
-        setStatusGothamist("error");
-      }
-    } catch (error) {
-      setStatusGothamist("error");
-    }
-  };
+      setStatusSahan("loading");
 
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    setStatusTheCity("loading");
-    setStatusGothamist("loading");
-
-    submitTheCity(e);
-    submitGothamist(e);
-  };
+      submitSahan(e, city);
+    };
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -114,7 +68,7 @@ export const NewsletterSignupBanner: React.FC<{
         className={classnames("container", "py-4", isOnLandingPage && "px-4")}
       >
         <form
-          onSubmit={handleSubmit}
+          onSubmit={handleSubmit(city)}
           className={classnames(
             "is-flex",
             !isOnLandingPage && "is-justify-content-center"
@@ -151,12 +105,9 @@ export const NewsletterSignupBanner: React.FC<{
                 <button
                   className="button is-small is-white mt-1"
                   type="submit"
-                  disabled={
-                    statusTheCity === "loading" || statusGothamist === "loading"
-                  }
+                  disabled={statusSahan === "loading"}
                 >
-                  {statusTheCity === "loading" ||
-                  statusGothamist === "loading" ? (
+                  {statusSahan === "loading" ? (
                     <>
                       Submitting
                       <span className="dot">.</span>
@@ -171,7 +122,7 @@ export const NewsletterSignupBanner: React.FC<{
             </div>
           </div>
         </form>
-        {statusTheCity === "success" && statusGothamist === "success" ? (
+        {statusSahan === "success" ? (
           <p
             className={classnames(
               "label mt-2",
@@ -180,33 +131,7 @@ export const NewsletterSignupBanner: React.FC<{
           >
             You're signed up!
           </p>
-        ) : statusTheCity === "success" && statusGothamist === "error" ? (
-          <p
-            className={classnames(
-              "label mt-2",
-              !isOnLandingPage && "has-text-centered"
-            )}
-          >
-            You're signed up with THE CITY! Something went wrong with Gothamist.{" "}
-            <OutboundLink to={GOTHAMIST_FALLBACK_NEWSLETTER_LINK}>
-              Sign up manually
-            </OutboundLink>
-            .
-          </p>
-        ) : statusTheCity === "error" && statusGothamist === "success" ? (
-          <p
-            className={classnames(
-              "label mt-2",
-              !isOnLandingPage && "has-text-centered"
-            )}
-          >
-            You're signed up with Gothamist! Something went wrong with THE CITY.{" "}
-            <OutboundLink to={THE_CITY_FALLBACK_NEWSLETTER_LINK}>
-              Sign up manually
-            </OutboundLink>
-            .
-          </p>
-        ) : statusTheCity === "error" && statusGothamist === "error" ? (
+        ) : statusSahan === "error" ? (
           <p
             className={classnames(
               "label mt-2",
@@ -214,12 +139,8 @@ export const NewsletterSignupBanner: React.FC<{
             )}
           >
             Something went wrong. Sign up manually via{" "}
-            <OutboundLink to={THE_CITY_FALLBACK_NEWSLETTER_LINK}>
-              THE CITY
-            </OutboundLink>{" "}
-            and{" "}
-            <OutboundLink to={GOTHAMIST_FALLBACK_NEWSLETTER_LINK}>
-              Gothamist
+            <OutboundLink to={SAHAN_FALLBACK_NEWSLETTER_LINK}>
+              Sahan Journal
             </OutboundLink>
             .
           </p>
